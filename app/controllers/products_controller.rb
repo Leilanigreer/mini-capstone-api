e1da4d43ad37c35e5689b7f48971b55150790573
Leilanigreer <leilani@lpcgolf.com>
2024-09-01 15:06:03 -0700

    changes from .create to .new and .save

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -10,10 +10,22 @@ class ProductsController < ApplicationController
     render template: "products/show"
   end
 
-  def create
+  # def create_hardcoded
   
-    @product = Product.create(name: "mango", price: 4, image_url: "https://www.svz.com/wp-content/uploads/2018/05/Mango.jpg", description: "Mangoes are egg-shaped or pear-shaped, and can be round, oval, heart, or kidney shaped. They are usually 3–5 inches long and have a thin, waxy skin that is red and green when unripe, and turns yellow, orange, purple, or red as it matures. The flesh is bright orange and soft, with a large, flat pit in the middle.", in_stock: 12)
+  #   @product = Product.create(name: "mango", price: 4, image_url: "https://www.svz.com/wp-content/uploads/2018/05/Mango.jpg", description: "Mangoes are egg-shaped or pear-shaped, and can be round, oval, heart, or kidney shaped. They are usually 3–5 inches long and have a thin, waxy skin that is red and green when unripe, and turns yellow, orange, purple, or red as it matures. The flesh is bright orange and soft, with a large, flat pit in the middle.", in_stock: 12)
     
+  #   render template: "products/show"
+  # end
+
+  def create 
+    @product = Product.new(
+      name: params[:name],
+      price: params[:price],
+      image_url: params[:image_url],
+      description: params[:description],
+      in_stock: params[:in_stock]
+    )
+    @product.save
     render template: "products/show"
-  end
+  end 
 end
