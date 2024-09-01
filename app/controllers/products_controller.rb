f569b777b937e06a65f32e0066ba7d80289de43f
Leilanigreer <leilani@lpcgolf.com>
2024-09-01 14:14:29 -0700

    added RESTful get methods for index(all) and show(single)

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,14 +1,12 @@
 class ProductsController < ApplicationController
 
-  def all 
-    @products = Product.all
-    render template: "products/index" 
+  def index 
+    @products = Product.all 
+    render template: "products/index"
   end
 
-  def a_product
-    @product = Product.find_by(id: params["id"])
+  def show
+    @product = Product.find_by(id: params[:id])
     render template: "products/show"
   end
-
-
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,10 +5,9 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
-  get "/all_products" => "products#all"
+  get "/products" => "products#index"
 
-  get "/first/:id" => "products#a_product"
-
+  get "/products/:id" => "products#show"
 
   # Defines the root path route ("/")
   # root "posts#index"
