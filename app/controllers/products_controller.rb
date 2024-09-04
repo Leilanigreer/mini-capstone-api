ed68e642d25a3796ddc1fa5a8981f9b66b50b0fe
Leilanigreer <leilani@lpcgolf.com>
2024-09-03 18:45:44 -0700

    added tests in test for index, create and show. Need to finish update and delete

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,6 +4,18 @@ class ProductsController < ApplicationController
     @products = Product.all 
     render template: "products/index"
   end
+  
+  def create 
+    @product = Product.new(
+      name: params[:name],
+      price: params[:price],
+      image_url: params[:image_url],
+      description: params[:description],
+      in_stock: params[:in_stock]
+    )
+    @product.save
+    render template: "products/show"
+  end 
 
   def show
     @product = Product.find_by(id: params[:id])
@@ -17,25 +29,14 @@ class ProductsController < ApplicationController
   #   render template: "products/show"
   # end
 
-  def create 
-    @product = Product.new(
-      name: params[:name],
-      price: params[:price],
-      image_url: params[:image_url],
-      description: params[:description],
-      in_stock: params[:in_stock]
-    )
-    @product.save
-    render template: "products/show"
-  end 
 
   def update
     @product = Product.find_by(id: params[:id])
-    @product.name = params[:name],
-    @product.price = params[:price],
-    @product.image_url = params[:image_url],
-    @product.description = params[:description],
-    @product.in_stock = params[:in_stock]
+    @product.name = params[:name] || @products.name,
+    @product.price = params[:price] || @product.price,
+    @product.image_url = params[:image_url] || @product.image_url,
+    @product.description = params[:description] || @product.description,
+    @product.in_stock = params[:in_stock] || @product.in_stock
     @product.save
     render template: "products/show"
   end
--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -1,7 +1,31 @@
 require "test_helper"
 
 class ProductsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
-end
+  
+  test "index" do
+    get "/products.json"
+    assert_response 200
+
+    data = JSON.parse(response.body)
+    assert_equal Product.count, data.length 
+  end
+
+  test "create" do
+    assert_difference "Product.count", 1 do
+      post "/products.json", params: { name: "cake", price: 300, image_url: "http://www.example.com:80products.json", description: "I love cake", in_stock: 1 }
+      assert_response 200
+    end
+  end
+
+  test "show" do
+    get "/products/#{Product.first.id}.json"
+    assert_response 200
+
+    data = JSON.parse(response.body)
+    assert_equal ["id", "name", "price", "image_url", "description", "in_stock"], data.keys
+  end
+
+#   test "update" do
+#     product = Product.first
+#     patch "/products/#{product.}"
+# end
