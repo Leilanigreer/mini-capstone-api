1929eaa3bfbd83feda8b0f95d896ffd5e7e4ed85
Leilanigreer <leilani@lpcgolf.com>
2024-09-04 17:09:20 -0700

    added 3 methods to the Product class. is_discounted? tax and total

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,17 @@
 class Product < ApplicationRecord
+  def is_discounted?
+    if price <= 10
+      discount = true
+    else 
+      false
+    end
+  end
+
+  def tax
+    price * 0.09
+  end
+
+  def total
+    tax + price 
+  end
 end
