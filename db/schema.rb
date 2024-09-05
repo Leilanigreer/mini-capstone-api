0584505f8ec0505762d46884db7720775bb70549
Leilanigreer <leilani@lpcgolf.com>
2024-09-05 16:24:44 -0700

    added quantity inventory

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_09_05_021424) do
+ActiveRecord::Schema[7.1].define(version: 2024_09_05_022138) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -22,6 +22,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_09_05_021424) do
     t.integer "in_stock"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "quantity_in_stock"
   end
 
 end
